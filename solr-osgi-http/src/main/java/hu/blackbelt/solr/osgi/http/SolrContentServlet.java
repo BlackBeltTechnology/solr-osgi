87f510e30dcea74da2260d72bd8013fd6311039a
Robert Csakany <robert.csakany@blackbelt.hu>
2018-08-29 12:58:06 +0200

    Fixing mime type and URL handling

--- solr-osgi-http/src/main/java/hu/blackbelt/solr/osgi/http/SolrContentServlet.java
+++ solr-osgi-http/src/main/java/hu/blackbelt/solr/osgi/http/SolrContentServlet.java
@@ -3,6 +3,7 @@ package hu.blackbelt.solr.osgi.http;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.output.CloseShieldOutputStream;
 
+import javax.activation.MimetypesFileTypeMap;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,6 +12,15 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class SolrContentServlet extends HttpServlet {
+
+    private MimetypesFileTypeMap mimetypesFileTypeMap;
+
+    public SolrContentServlet() {
+        ClassLoader old = Thread.currentThread().getContextClassLoader();
+        Thread.currentThread().setContextClassLoader(this.getClass().getClassLoader());
+        mimetypesFileTypeMap = new MimetypesFileTypeMap();
+        Thread.currentThread().setContextClassLoader(old);
+    }
 
     @Override
     public void doGet(HttpServletRequest request,
@@ -21,30 +31,35 @@ public class SolrContentServlet extends HttpServlet {
 
         // This attribute is set by the SolrDispatchFilter
         String admin = request.getRequestURI().substring(request.getContextPath().length());
+        if (admin == null || admin.equalsIgnoreCase("") || admin.equals("/")) {
+            admin = "/index.html";
+        }
 
         InputStream in = getServletContext().getResourceAsStream(admin);
         OutputStream out = new CloseShieldOutputStream(response.getOutputStream());
 
         if (in != null) {
             try {
-                    response.setCharacterEncoding("UTF-8");
-                    if (admin.endsWith(".css")) {
-                        response.setContentType("text/css");
-                    } else if (admin.endsWith(".html")) {
-                        response.setContentType("text/html");
-                    } else if (admin.endsWith(".png")) {
-                        response.setContentType("image/png");
-                    } else if (admin.endsWith(".gif")) {
-                        response.setContentType("image/gif");
-                    } else if (admin.endsWith(".swf")) {
-                        response.setContentType("application/x-shockwave-flash");
-                    } else if (admin.endsWith(".swf")) {
-                        response.setContentType("application/javascript");
-                    } else {
-                        response.setContentType("application/octet-stream");
-                    }
-
-                    IOUtils.copy(in, out);
+                response.setCharacterEncoding("UTF-8");
+                // response.setContentType(mimetypesFileTypeMap.getContentType(admin));
+                if (admin.endsWith(".css")) {
+                    response.setContentType("text/css");
+                } else if (admin.endsWith(".html")) {
+                    response.setContentType("text/html");
+                } else if (admin.endsWith(".png")) {
+                    response.setContentType("image/png");
+                } else if (admin.endsWith(".gif")) {
+                    response.setContentType("image/gif");
+                } else if (admin.endsWith(".swf")) {
+                    response.setContentType("application/x-shockwave-flash");
+                } else if (admin.endsWith(".js")) {
+                    response.setContentType("application/javascript");
+                } else if (admin.endsWith(".svg")) {
+                    response.setContentType("image/svg+xml");
+                } else {
+                    response.setContentType("application/octet-stream");
+                }
+                IOUtils.copy(in, out);
             } finally {
                 IOUtils.closeQuietly(in);
                 IOUtils.closeQuietly(out);
